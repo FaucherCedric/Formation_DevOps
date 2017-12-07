61f5c45a86edfe50efac407605c772f1a40cb56f
Cedric  Faucher <faucher.cedric@hotmail.fr>
2017-12-07 11:42:14 +0100

    change permission file

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_abebfa6e-7f18-4f7d-9d04-c35ebc4dda59.git
+git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push-par-clevercloud-customers.services.clever-cloud.com/app_abebfa6e-7f18-4f7d-9d04-c35ebc4dda59.git
 git push cleverapps master --verbose --force
